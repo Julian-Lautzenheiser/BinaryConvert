0fff9d526956cc4609c314dafa41269ecace12d5
Julian Lautzenheiser <Julian.R.Lautzenheiser-1@ou.edu>
2023-06-21 15:28:19 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace Binary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboxMode = new System.Windows.Forms.ComboBox();
+            this.lblMode = new System.Windows.Forms.Label();
+            this.tboxInput = new System.Windows.Forms.TextBox();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.buttTrans = new System.Windows.Forms.Button();
+            this.tboxOutput = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cboxMode
+            // 
+            this.cboxMode.FormattingEnabled = true;
+            this.cboxMode.Items.AddRange(new object[] {
+            "Binary to Decimal",
+            "Binary to Octal",
+            "Binary to Hexadecimal",
+            "Decimal to Binary",
+            "Decimal to Octal",
+            "Decimal to Hexadecimal",
+            "Octal to Binary",
+            "Octal to Decimal",
+            "Octal to Hexadecimal",
+            "Hexadecimal to Binary",
+            "Hexadecimal to Octal",
+            "Hexadecimal to Decimal",
+            "Binary to Alphanumerical",
+            "Octal to Alphanumerical",
+            "Hexadecimal to Alphanumerical",
+            "Alphanumerical to Binary",
+            "Alphanumerical to Octal",
+            "Alphanumerical to Hexadecimal"});
+            this.cboxMode.Location = new System.Drawing.Point(214, 69);
+            this.cboxMode.Name = "cboxMode";
+            this.cboxMode.Size = new System.Drawing.Size(184, 21);
+            this.cboxMode.TabIndex = 0;
+            // 
+            // lblMode
+            // 
+            this.lblMode.AutoSize = true;
+            this.lblMode.Location = new System.Drawing.Point(171, 72);
+            this.lblMode.Name = "lblMode";
+            this.lblMode.Size = new System.Drawing.Size(37, 13);
+            this.lblMode.TabIndex = 1;
+            this.lblMode.Text = "Mode:";
+            // 
+            // tboxInput
+            // 
+            this.tboxInput.Location = new System.Drawing.Point(214, 97);
+            this.tboxInput.Name = "tboxInput";
+            this.tboxInput.Size = new System.Drawing.Size(184, 20);
+            this.tboxInput.TabIndex = 2;
+            this.tboxInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tboxInput_KeyPress);
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(173, 103);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(34, 13);
+            this.lblInput.TabIndex = 3;
+            this.lblInput.Text = "Input:";
+            // 
+            // buttTrans
+            // 
+            this.buttTrans.Location = new System.Drawing.Point(269, 141);
+            this.buttTrans.Name = "buttTrans";
+            this.buttTrans.Size = new System.Drawing.Size(75, 23);
+            this.buttTrans.TabIndex = 4;
+            this.buttTrans.Text = "Translate";
+            this.buttTrans.UseVisualStyleBackColor = true;
+            this.buttTrans.Click += new System.EventHandler(this.buttTrans_Click);
+            // 
+            // tboxOutput
+            // 
+            this.tboxOutput.BackColor = System.Drawing.SystemColors.Window;
+            this.tboxOutput.Location = new System.Drawing.Point(214, 196);
+            this.tboxOutput.Name = "tboxOutput";
+            this.tboxOutput.ReadOnly = true;
+            this.tboxOutput.Size = new System.Drawing.Size(184, 20);
+            this.tboxOutput.TabIndex = 5;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(166, 203);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(42, 13);
+            this.lblOutput.TabIndex = 6;
+            this.lblOutput.Text = "Output:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 312);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.tboxOutput);
+            this.Controls.Add(this.buttTrans);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.tboxInput);
+            this.Controls.Add(this.lblMode);
+            this.Controls.Add(this.cboxMode);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboxMode;
+        private System.Windows.Forms.Label lblMode;
+        private System.Windows.Forms.TextBox tboxInput;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Button buttTrans;
+        private System.Windows.Forms.TextBox tboxOutput;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Binary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+		private void tboxInput_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			char ch = e.KeyChar;
+			if ((cboxMode.SelectedIndex >= 0 && cboxMode.SelectedIndex < 3) || cboxMode.SelectedIndex == 12)
+			{ //Only allow 1 and 0 when binary is selected in combo box
+				if (ch != 48 && ch != 49)
+				{
+					e.Handled = true;
+					MessageBox.Show("Binary is contained to only 1 and 0");
+				}
+			}
+		}
+        private void buttTrans_Click(object sender, EventArgs e)
+		{
+			int mode = cboxMode.SelectedIndex;
+			String input = tboxInput.Text;
+			String output = "";
+
+			switch (mode) { 
+            case 0: //Binary to Decimal 
+					output = binaryToDecimal(input);
+				break;
+			case 1: //Binary to Octal
+					output = binaryToHigher(input, 3);
+				break;
+			case 2: //Binary to Hexadecimal
+					output = binaryToHigher(input, 4);
+				break;
+			case 3: //Decimal to Binary
+				break;
+			case 4: //Decimal to Octal
+				break;
+			case 5: //Decimal to Hexadecimal
+				break;
+			case 6: //Octal to Binary
+				break;
+			case 7: //Octal to Decimal
+				break;
+			case 8: //Octal to Hexadecimal
+				break;
+			case 9: //Hexadecimal to Binary
+				break;
+			case 10: //Hexadecimal to Octal
+				break;
+			case 11: //Hexadecimal to Decimal
+				break;
+			case 12: //Binary to Alphanumerical
+				break;
+			case 13: //Octal to Alphanumerical
+				break;
+			case 14: //Hexadecimal to Alphanumerical
+				break;
+			case 15: //Alphanumerical to Binary
+				break;
+			case 16: //Alphanumerical to Octal
+				break;
+			case 17: //Alphanumerical to Hexadecimal
+				break;
+			default:
+				break;
+			}
+			tboxOutput.Text = output;
+		}
+
+		private String binaryToDecimal(String input)
+        {
+			int sum = 0;
+			int mult = 1;
+
+			for (int i = input.Length - 1; i >= 0; --i)
+            {
+				sum += mult * ((int)input[i] - 48);
+				mult *= 2;
+            }
+
+			return sum.ToString();
+        }
+
+		private String binaryToHigher(String input, int power)
+        {
+			String output = "";
+			int tempSum = 0;
+			int mult = 1;
+			int powerCount = 1;
+			Dictionary<int, char> hex = new Dictionary<int, char>(); ;
+			if (power == 4) //Creates dict for hex logic of #s > 9
+			{
+				hex.Add(10, 'A');
+				hex.Add(11, 'B');
+				hex.Add(12, 'C');
+				hex.Add(13, 'D');
+				hex.Add(14, 'E');
+				hex.Add(15, 'F');
+			}
+
+			for (int i = input.Length - 1; i >= 0; --i)
+			{
+				tempSum += mult * ((int)input[i] - 48);
+				if (powerCount == power) //Resets multiplyer and sum and adds sum to the output
+				{
+					if (tempSum > 9) //Inputs a letter for numbers higher than 9
+                    {
+						output = hex[tempSum] + output;
+                    } else
+                    {
+						output = "" + tempSum + output;
+					}
+					powerCount = 1;
+					mult = 1;
+					tempSum = 0;
+				}
+				else
+				{
+					mult *= 2;
+					powerCount++;
+					if (i == 0) //Makes sure full number is outputted if not ending on last
+					{
+						output = "" + tempSum + output;
+					}
+				}
+			}
+			return output;
+        }
+
+		private String decimalToBinary(String input)
+        {
+			int num = int.Parse(input);
+
+			return "";
+        }
+        
+    }
+}
